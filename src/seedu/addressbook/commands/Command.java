f0e9f2fdb62bc15050b358ca4762d3cf1b415909
nawri14 <nawri14@gmail.com>
2016-09-15 00:42:01 +0800

    Added isMutating method in all execute command class

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,9 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /* Check every Execute command that it need to be save or not*/
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
